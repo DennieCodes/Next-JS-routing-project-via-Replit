f40abb428f78b8e87c2c08fedffe00524efcc5ff
Dennie <oculareo@gmail.com>
2022-05-23 00:44:28 +0000

    Added buttons and icons to EventItem component

--- components/events/EventItem.js
+++ components/events/EventItem.js
@@ -1,5 +1,8 @@
-import Link from 'next/link';
 import styles from './EventItem.module.css';
+import Button from '../ui/button';
+import DateIcon from '../icons/date-icon';
+import AddressIcon from '../icons/address-icon';
+import ArrowRightIcon from '../icons/arrow-right-icon';
 
 export default function EventItem(props) {
 	const { title, image, date, location, id } = props;
@@ -12,23 +15,30 @@ export default function EventItem(props) {
 
 	const formattedAddress = location.replace(', ', '\n');
 	const exploreLink = `/events/${id}`;
-	
+
 	return (
 		<li className={styles.item}>
 			<img src={'/' + image} alt={title} />
-			<div>
-				<div>
+			<div className={styles.content}>
+				<div className={styles.summary}>
 					<h2>{title}</h2>
-					<div>
+					<div className={styles.date}>
+						<DateIcon />
 						<time>{humanReadableDate}</time>
 					</div>
-					<div>
+					<div className={styles.address}>
+						<AddressIcon />
 						<address>{formattedAddress}</address>
 					</div>
 				</div>
 
-				<div>
-					<Link href={exploreLink}>Explore Event</Link>
+				<div className={styles.actions}>
+					<Button link={exploreLink}>
+						<span>Explore Event</span>
+						<span className={styles.icon}>
+							<ArrowRightIcon />
+						</span>
+					</Button>
 				</div>
 			</div>
 		</li>
