dd1442164d86205d112f55d29397cc33797ea0a0
Dennie <oculareo@gmail.com>
2022-05-27 12:39:27 +0000

    Added EventSearch Component to Event index page

--- components/ui/button.js
+++ components/ui/button.js
@@ -2,9 +2,15 @@ import Link from 'next/link';
 import styles from './button.module.css';
 
 export default function Button(props) {
+	
 	return (
+		props.link ?
 		<Link href={props.link}>
 			<a className={styles.btn}>{props.children}</a>
 		</Link>
+		:
+		<button className={styles.btn} onClick={props.onClick}>
+			{props.children}
+		</button>
 	);
 }
